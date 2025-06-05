0050064cd7940a13a94a192bc021e2a1bc8ad24b
André Nicoletti <andregnicoletti@gmail.com>
2025-06-05 09:35:11 -0300

    interpolações

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  tipo: string = 'Teste';
+  preco: number = 1000;
 
+  plano = {
+    infos: {
+      tipo: 'outro exemplo'
+    },
+  };
+
+  getFullPrice() {
+    setTimeout(() => {
+      console.log('Set Timeout');
+      this.preco = 1234;
+    }, 4000);
+
+    return 'R$' + this.preco + ',00/Mês';
+  }
 }
