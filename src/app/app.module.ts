28b06ef81e4c9c70dd864d0843793cb42096b230
André Nicoletti <andregnicoletti@gmail.com>
2025-06-05 09:22:41 -0300

    componentes de outro módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
-import { CardRoxoComponent } from './components/card-roxo/card-roxo.component';
-import { CardButtonComponent } from './components/card-button/card-button.component';
-import { CardRoxoButtonComponent } from './components/card-roxo-button/card-roxo-button.component';
+import { CardsModule } from './modules/cards/cards.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CardRoxoComponent,
-    CardButtonComponent,
-    CardRoxoButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
