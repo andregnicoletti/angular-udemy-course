e1461ae4da082b0e9df9b663591200812223dc1d
André Nicoletti <andregnicoletti@gmail.com>
2025-06-05 09:05:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { CardRoxoComponent } from './components/card-roxo/card-roxo.component';
+import { CardButtonComponent } from './components/card-button/card-button.component';
+import { CardRoxoButtonComponent } from './components/card-roxo-button/card-roxo-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
   ],
   imports: [
     BrowserModule
